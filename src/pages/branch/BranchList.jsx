8a6019c130f523909bb21290b9ab17eb37f1ea21
Hmue Thet Paing Kyaw <94979276+hmuethetpaingkyaw@users.noreply.github.com>
2023-02-07 14:20:57 +0630

    delete confirm dialog

--- src/pages/branch/BranchList.jsx
+++ src/pages/branch/BranchList.jsx
@@ -1,11 +1,5 @@
 import { useEffect, useState } from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+
 import {
   Button,
   Checkbox,
@@ -23,15 +17,14 @@ import { Box } from "@mui/system";
 import AddCircleRoundedIcon from "@mui/icons-material/AddCircleRounded";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 import { withStyles } from "@material-ui/core/styles";
-import DriveFileRenameOutlineRoundedIcon from "@mui/icons-material/DriveFileRenameOutlineRounded";
-import DeleteForeverRoundedIcon from "@mui/icons-material/DeleteForeverRounded";
-import DisplaySettingsRoundedIcon from "@mui/icons-material/DisplaySettingsRounded";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../../components/navbar/Navbar";
 import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
 import * as BranchService from "./../../services/branchService";
 import { setBranches, deleteBranch } from "../../store/reducer.branch";
+import ConfirmDialog from "../../components/Dialogs/ConfirmDialog";
+import List from "./List";
 
 const CssTextField = withStyles({
   root: {
@@ -59,6 +52,8 @@ const BranchList = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
   const [open, setOpen] = useState(false);
+  const [showDelete, setShowDelete] = useState(false);
+  const [editData, setEditData] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -266,93 +261,28 @@ const BranchList = () => {
               <Box>{t("new")}</Box>
             </Button>
           </Box>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead sx={{ backgroundColor: "#094708" }}>
-                <TableRow>
-                  <TableCell
-                    sx={{ color: "white", fontSize: "16px" }}
-                    align="center"
-                  >
-                    {t("no")}
-                  </TableCell>
-                  <TableCell
-                    sx={{ color: "white", fontSize: "16px" }}
-                    align="center"
-                  >
-                    {" "}
-                    {t("branch.name")}
-                  </TableCell>
-                  <TableCell
-                    sx={{ color: "white", fontSize: "16px" }}
-                    align="center"
-                  >
-                    {" "}
-                    {t("branch.staff_qty")}
-                  </TableCell>
-                  <TableCell
-                    sx={{ color: "white", fontSize: "16px" }}
-                    align="center"
-                  >
-                    {t("phone")}
-                  </TableCell>
-                  <TableCell
-                    sx={{ color: "white", fontSize: "16px" }}
-                    align="center"
-                  >
-                    {t("address")}
-                  </TableCell>
-                  <TableCell
-                    sx={{ color: "white", fontSize: "16px" }}
-                    align="center"
-                  >
-                    {t("action")}
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {branches?.map((row, index) => (
-                  <TableRow
-                    key={row.id}
-                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                  >
-                    <TableCell component="th" scope="row" align="center">
-                      {index + 1}
-                    </TableCell>
-                    <TableCell align="center">{row.name}</TableCell>
-                    <TableCell align="center">{0}</TableCell>
-                    <TableCell align="center">{row.phone}</TableCell>
-                    <TableCell align="center">{row.address}</TableCell>
-                    <TableCell align="center">
-                      <DisplaySettingsRoundedIcon
-                        onClick={handleClickOpen}
-                        sx={{ color: "green", fontSize: "25px" }}
-                      />
-                      <DriveFileRenameOutlineRoundedIcon
-                        onClick={() => handleEdit(row)}
-                        sx={{
-                          color: "#36353d",
-                          fontSize: "25px",
-                          marginLeft: "5px",
-                        }}
-                      />
-                      <DeleteForeverRoundedIcon
-                        onClick={() => {
-                          handleDelete(row.id);
-                        }}
-                        sx={{
-                          color: "red",
-                          fontSize: "25px",
-                          marginLeft: "5px",
-                        }}
-                      />
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+          <List
+            data={branches}
+            handleClickOpen={handleClickOpen}
+            handleEdit={handleEdit}
+            onDelete={(row) => {
+              setEditData(row);
+              setShowDelete(true);
+            }}
+          />
         </Box>
+
+        {showDelete && (
+          <ConfirmDialog
+            title={`Delete Branch`}
+            body={`Are you sure to delete ${editData?.name}?`}
+            onToggle={() => setShowDelete(false)}
+            onConfirm={() => {
+              setShowDelete(false);
+              handleDelete(editData?.id);
+            }}
+          />
+        )}
       </div>
     </>
   );
---
+++ src/pages/roleAndAccess/List.jsx
@@ -0,0 +1,75 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { useTranslation } from "react-i18next";
+import DriveFileRenameOutlineRoundedIcon from "@mui/icons-material/DriveFileRenameOutlineRounded";
+import DeleteForeverRoundedIcon from "@mui/icons-material/DeleteForeverRounded";
+import { useNavigate } from "react-router-dom";
+
+function List({ data, handleClickOpen, handleEdit, onDelete }) {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead sx={{ backgroundColor: "#094708" }}>
+          <TableRow>
+            <TableCell sx={{ color: "white", fontSize: "16px" }} align="center">
+              {t("no")}
+            </TableCell>
+            <TableCell sx={{ color: "white", fontSize: "16px" }} align="center">
+              {t("name")}
+            </TableCell>
+            <TableCell sx={{ color: "white", fontSize: "16px" }} align="center">
+              {t("position")}
+            </TableCell>
+
+            <TableCell sx={{ color: "white", fontSize: "16px" }} align="center">
+              {t("action")}
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data?.map((row, index) => (
+            <TableRow
+              key={row.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row" align="center">
+                {index + 1}
+              </TableCell>
+              <TableCell align="center">{row.name}</TableCell>
+              <TableCell align="center">{row.position}</TableCell>
+              <TableCell align="center">
+                <DriveFileRenameOutlineRoundedIcon
+                  onClick={() => {
+                    navigate("/admin/edit-role-access/" + row.id);
+                  }}
+                  sx={{
+                    color: "#36353d",
+                    fontSize: "25px",
+                    marginLeft: "5px",
+                  }}
+                />
+                <DeleteForeverRoundedIcon
+                  onClick={()=> onDelete(row)}
+                  sx={{
+                    color: "red",
+                    fontSize: "25px",
+                    marginLeft: "5px",
+                  }}
+                />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+export default List;
